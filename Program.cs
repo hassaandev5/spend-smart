cb77fa6fd3cfcca665b6321d7bd49ff8de75b97d
Hassaan Ahmad <62022834+hassaandev5@users.noreply.github.com>
2024-06-18 23:51:27 +0500

    modified: Program.cs(testing git)

--- Program.cs
+++ Program.cs
@@ -4,11 +4,16 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        // Create a builder object which will be used to configure the application and its services.
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
+        // Add ControllersWithViews to the service collection. This will add ASP.NET Core MVC services and configure
+        // options for controllers with views. ControllersWithViews means that the application will use views
+        // for its responses.
         builder.Services.AddControllersWithViews();
-
+        
+        // Build the application. This will create an instance of the application based on the configuration that was
+        // created by the builder.
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
