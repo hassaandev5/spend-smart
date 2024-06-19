7a63253b4bd7a06f4752f929b8cc61c737c7288d
Hassaan Ahmad <62022834+hassaandev5@users.noreply.github.com>
2024-06-20 02:38:11 +0500

    added a view and modified controller and Index view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,16 +17,21 @@ public class HomeController : Controller
     {
             return View();
     }
+    public IActionResult Expenses()
+    {
+        return View();
+    }
+    public IActionResult CreateEditExpense()
+    {
+        return View();
+    }
 
     public IActionResult Privacy()
     {
         return View();
     }
 
-    public IActionResult Expenses()
-    {
-        return View();
-    }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
